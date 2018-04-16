54f9747f05be2a420e8b19af1b696512c597f8c1
Raul Neis <raulneis@gmail.com>
2018-04-16 18:08:48 -0300

    get-xampp.sh

--- get-xampp.sh
+++ get-xampp.sh
@@ -1,5 +1,2 @@
-
-docker-compose up -d
-docker-compose stop
-
+#!/bin/bash
 wget -O .docker/xampp-linux-x64-1.8.2-6-installer.run https://sourceforge.net/projects/xampp/files/XAMPP%20Linux/1.8.2/xampp-linux-x64-1.8.2-6-installer.run/download
